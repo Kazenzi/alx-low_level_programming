5243bec6224659808c121d984b286df5aca6b604
Kazenzi <jacintakazenzi99@gmail.com>
2022-04-12 12:40:05 -0700

    0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,42 @@
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet - Entry point
+ *
+ *   * Return:0
+ *
+ *    */
+
+
+
+void print_alphabet(void)
+
+{
+
+		char c = 'a';
+
+
+
+			while (c <= 'z')
+
+					{
+
+								_putchar(c);
+
+										c++;
+
+
+
+											}
+
+				_putchar('\n');
+
+
+
+}
+
+
