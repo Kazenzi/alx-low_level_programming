aff5cfdb6a3cd8ce37ec939854a9af2d1d252e32
Kazenzi <jacintakazenzi99@gmail.com>
2022-04-15 09:18:43 -0700

    supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper-check if its upper case
+ * @c:ascii int value of the character to check
+ * Return: 1 if  true else  0
+ */
+int _isupper (int c)
+{
+	if (c>=65 && c <=90)
+		return (1);
+	else
+		return (0);
+}
