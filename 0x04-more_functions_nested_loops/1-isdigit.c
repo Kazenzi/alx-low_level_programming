340356571b00d4b751fecc5c786f3af966232381
Kazenzi <jacintakazenzi99@gmail.com>
2022-04-16 04:16:13 -0700

    zz

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,41 +1,18 @@
 #include "main.h"
+/**
+ * _isdigit - check if is digit.
+ *
+ * @c: ascii value to check
+ *
+ * Return: 1 if true else 0
+*/
+int _isdigit(int c)
 
-	
-
-
-
-	/**
-	 *
-	 * 	* _isdigit - check if is digit.
-	 *
-	 * 		* @c: ascii value to check
-	 *
-	 * 			*
-	 *
-	 * 				* Return: 1 if true else 0
-	 *
-	 * 					*/
-
-	
-
-
-
-	int _isdigit(int c)
-
-		{
-
-				if (c >= 48 && c <= 57)
-
-						return (1);
-
-					
-
-
-
-					else
-
-							return (0);
-
-						}
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
 
 
