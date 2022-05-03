e2565b7ad6ece8b42bf1e9567bc46078595b0190
Kazenzi <jacintakazenzi99@gmail.com>
2022-05-03 08:39:41 -0700

    stt

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,3 +1,14 @@
 #include "main.h"
+/**
+ * _isupper - check if uppercase.
+ * @c: ascii int value of the character to check
+ *
+ * Return: 1 if true else 0
+ */
 int _isupper(int c)
-{}
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,3 +1,17 @@
 #include "main.h"
+/**
+ * _strlen - length of a string by pointer
+ * @s: input char pointer
+ * Return: length of a string
+ */
 int _strlen(char *s)
-{}
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
